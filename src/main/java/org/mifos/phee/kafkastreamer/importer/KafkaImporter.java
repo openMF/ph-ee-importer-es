29065447d7e3600499584eb81ab24b832aa81690
Anover000 <ankit@fynarfin.io>
2024-05-13 17:25:15 +0530

    es compatible dateTime format

--- src/main/java/org/mifos/phee/kafkastreamer/importer/KafkaImporter.java
+++ src/main/java/org/mifos/phee/kafkastreamer/importer/KafkaImporter.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;import java.text.SimpleDateFormat;import java.util.Date;
+import org.springframework.stereotype.Component;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class KafkaImporter {
@@ -32,7 +34,7 @@ public class KafkaImporter {
         }
 
         JSONObject data = new JSONObject(rawData);
-        data.put("importedTime", new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
+        data.put("importedTime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ").format(new Date()));
         logger.trace("from kafka: {}", data.toString(2));
 
         elasticsearchClient.index(data);
